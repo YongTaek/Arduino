dcaee4841234b75f17a11cc344af18c7051a4822
OhYongTaek <ka123ak1@gmail.com>
2016-06-27 15:41:44 +0900

    bluetooth test [수정] MainActivity에서 블루투스 연결이 안되면 끊기도록 수정, 연결 될 경우 시리얼을 통해 숫자 송신이 가능하도록 함

--- app/src/main/java/com/example/ohyongtaek/arduino/MainActivity.java
+++ app/src/main/java/com/example/ohyongtaek/arduino/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.ohyongtaek.arduino;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +15,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    final public int REQUEST_ENABLE_BT = 1;
+
+    Set<BluetoothDevice> mDevices;
+    int mPairDeviceCount;
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothDevice mRemoteDevice;
+    BluetoothSocket mSocket;
+    InputStream mInputStream;
+    OutputStream mOutputStream;
+    char mDelimiter = '\n';
+    int readBufferPosition;
+    byte[] readBuffer;
+    EditText editText, sendText;
+    Button sendBtn, clearBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +58,41 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        sendText = (EditText) findViewById(R.id.send_text);
+        editText = (EditText) findViewById(R.id.receive_text);
+        sendBtn = (Button) findViewById(R.id.send_btn);
+        clearBtn = (Button) findViewById(R.id.clear_btn);
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter != null) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                selectDevice();
+            }
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_ENABLE_BT:
+                if (resultCode == RESULT_OK) {
+                    selectDevice();
+                } else if (resultCode == RESULT_CANCELED) {
+                    finish();
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -48,5 +115,130 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    void selectDevice() {
+        mDevices = mBluetoothAdapter.getBondedDevices();
+        mPairDeviceCount = mDevices.size();
+
+        if (mPairDeviceCount == 0) {
+            finish();
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("블루투스 장치 선택");
+
+        List<String> listItems = new ArrayList<>();
+        for (BluetoothDevice device : mDevices) {
+            listItems.add(device.getName());
+        }
+        listItems.add("취소");
+
+        final CharSequence[] items = listItems.toArray(new CharSequence[listItems.size()]);
+
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (which == mPairDeviceCount) {
+                    finish();
+                } else {
+                    connectToSelectedDevices(items[which].toString());
+                    sendBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            sendData(sendText.getText().toString());
+                            sendText.setText("");
+                        }
+                    });
+                }
+            }
+        });
+        builder.setCancelable(false);
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    BluetoothDevice getDeviceFromBondedList(String name) {
+        BluetoothDevice selectedDevice = null;
+        for (BluetoothDevice device : mDevices) {
+            if (name.equals(device.getName())) {
+                selectedDevice = device;
+                break;
+            }
+        }
+        return selectedDevice;
+    }
+
+    void connectToSelectedDevices(String selectedDeviceName) {
+        mRemoteDevice = getDeviceFromBondedList(selectedDeviceName);
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        try {
+            mSocket = mRemoteDevice.createRfcommSocketToServiceRecord(uuid);
+            mSocket.connect();
+
+            mOutputStream = mSocket.getOutputStream();
+            mInputStream = mSocket.getInputStream();
+            beginListenForData();
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, e.getStackTrace().toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    void beginListenForData() {
+        final Handler handler = new Handler();
+
+        readBuffer = new byte[1024];
+        readBufferPosition = 0;
+
+        Thread mWorkerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        int bytesAvailable = mInputStream.available();
+                        if (bytesAvailable > 0) {
+                            byte[] packtebBytes = new byte[bytesAvailable];
+                            mInputStream.read(packtebBytes);
+                            for (int i = 0; i < bytesAvailable; i++) {
+                                byte b = packtebBytes[i];
+                                Toast.makeText(MainActivity.this, b, Toast.LENGTH_SHORT).show();
+                                if (((char) b) == mDelimiter) {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            editText.setText(data);
+                                        }
+                                    });
+                                } else {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "데이터 수신 중 오류", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        mWorkerThread.start();
+    }
+
+    void sendData(String msg) {
+        msg += mDelimiter;
+        try {
+            mOutputStream.write(msg.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "데이터 송신 중 오류", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
